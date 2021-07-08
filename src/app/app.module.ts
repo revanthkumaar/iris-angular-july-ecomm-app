7d54bb169087894f5cd05042feb9b5d094fdb0d0
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-08 16:46:29 +0530

    material code corrected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'
-
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule //hhtp client service can be used across the app
+    HttpClientModule,
+    BrowserAnimationsModule, //hhtp client service can be used across the app
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { 
-
-  
-}
+export class AppModule {}
